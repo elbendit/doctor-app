fabd1ba57b84a5362e5cb77cda8156ae95b224ff
elbendit <misterjuli2001@gmail.com>
2022-12-22 01:48:39 -0500

    add color and responsive of search

--- doctor_app/lib/pages/home/widgets/search.dart
+++ doctor_app/lib/pages/home/widgets/search.dart
@@ -8,7 +8,6 @@ class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         Padding(
