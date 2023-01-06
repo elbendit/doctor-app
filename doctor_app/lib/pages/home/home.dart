c9815e77d392c1c5ac43ccd4862f2e7d503f945a
elbendit <misterjuli2001@gmail.com>
2023-01-05 20:39:29 -0500

    Arreglitos

--- doctor_app/lib/pages/home/home.dart
+++ doctor_app/lib/pages/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:doctor_app/common_widgets/listviewHo.dart';
+import 'package:doctor_app/common_widgets/sectionCategory.dart';
 import 'package:doctor_app/common_widgets/symptomsChip.dart';
 import 'package:doctor_app/common_widgets/verticalCard.dart';
 import 'package:flutter/material.dart';
@@ -26,10 +27,116 @@ class HomeContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: [
-      TopSection(),
-      MiddleSection(),
-      SectionNavigatorHome(),
-    ]);
+    return ListView(
+      children: [
+        TopSection(),
+        Column(mainAxisSize: MainAxisSize.min, children: [
+          SectionCategory(
+              subtitle: "Symptoms",
+              linkText: "See all",
+              widgetList: _getSymptomsChips,
+              heightListView: 80),
+          SectionCategory(
+              subtitle: "Favourite doctor",
+              linkText: "See all",
+              widgetList: _getDoctorCards,
+              heightListView: 200),
+          SectionCategory(
+              subtitle: "Favourite doctor",
+              linkText: "See all",
+              widgetList: _getDoctorCards,
+              heightListView: 200),
+        ]),
+        SectionNavigatorHome()
+      ],
+    );
   }
+}
+
+List<Widget> get _getSymptomsChips {
+  return [
+    SymptomsChip(identificationSymptomsChip: "Snuffle 🤧 "),
+    SymptomsChip(identificationSymptomsChip: "High Fever 🤒️"),
+    SymptomsChip(identificationSymptomsChip: "Nauseous 🤮"),
+    SymptomsChip(identificationSymptomsChip: "Snuffle 🤧 "),
+    SymptomsChip(identificationSymptomsChip: "High Fever 🤒️"),
+    SymptomsChip(identificationSymptomsChip: "Nauseous 🤮")
+  ];
+}
+
+List<Widget> get _getDoctorCards {
+  return [
+    VerticalCard(
+        imageUrl:
+            "https://img.freepik.com/vector-gratis/fondo-personaje-doctor_1270-84.jpg?w=2000",
+        name: "name",
+        rating: 4.5,
+        location: "location",
+        cardHeight: 200,
+        cardWidth: 200),
+    VerticalCard(
+        imageUrl:
+            "https://img.freepik.com/vector-gratis/fondo-personaje-doctor_1270-84.jpg?w=2000",
+        name: "name",
+        rating: 4.5,
+        location: "location",
+        cardHeight: 200,
+        cardWidth: 200),
+    VerticalCard(
+        imageUrl:
+            "https://img.freepik.com/vector-gratis/fondo-personaje-doctor_1270-84.jpg?w=2000",
+        name: "name",
+        rating: 4.5,
+        location: "location",
+        cardHeight: 200,
+        cardWidth: 200),
+    VerticalCard(
+        imageUrl:
+            "https://img.freepik.com/vector-gratis/fondo-personaje-doctor_1270-84.jpg?w=2000",
+        name: "name",
+        rating: 4.5,
+        location: "location",
+        cardHeight: 200,
+        cardWidth: 200),
+    VerticalCard(
+        imageUrl:
+            "https://img.freepik.com/vector-gratis/fondo-personaje-doctor_1270-84.jpg?w=2000",
+        name: "name",
+        rating: 4.5,
+        location: "location",
+        cardHeight: 200,
+        cardWidth: 200),
+    VerticalCard(
+        imageUrl:
+            "https://img.freepik.com/vector-gratis/fondo-personaje-doctor_1270-84.jpg?w=2000",
+        name: "name",
+        rating: 4.5,
+        location: "location",
+        cardHeight: 200,
+        cardWidth: 200),
+    VerticalCard(
+        imageUrl:
+            "https://img.freepik.com/vector-gratis/fondo-personaje-doctor_1270-84.jpg?w=2000",
+        name: "name",
+        rating: 4.5,
+        location: "location",
+        cardHeight: 200,
+        cardWidth: 200),
+    VerticalCard(
+        imageUrl:
+            "https://img.freepik.com/vector-gratis/fondo-personaje-doctor_1270-84.jpg?w=2000",
+        name: "name",
+        rating: 4.5,
+        location: "location",
+        cardHeight: 200,
+        cardWidth: 200),
+    VerticalCard(
+        imageUrl:
+            "https://img.freepik.com/vector-gratis/fondo-personaje-doctor_1270-84.jpg?w=2000",
+        name: "name",
+        rating: 4.5,
+        location: "location",
+        cardHeight: 200,
+        cardWidth: 200)
+  ];
 }
