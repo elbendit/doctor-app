006f67e03dd58bf5110f518a03440b49a8a05ae7
jsuarez <misterjuli2001@gmail.com>
2023-01-10 16:30:45 -0500

    progress doctor screen preview

--- doctor_app/lib/common_widgets/progressDoctor.dart
+++ doctor_app/lib/common_widgets/progressDoctor.dart
@@ -7,12 +7,55 @@ class ProgressDoctor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Row(
+      child: Column(
         children: [
-          InfoIcon(iron: "Star", cantidad: 400, indication: "indication"),
-          InfoIcon(iron: "iron", cantidad: 400, indication: "indication"),
-          InfoIcon(iron: "iron", cantidad: 400, indication: "indication"),
-          InfoIcon(iron: "iron", cantidad: 400, indication: "indication")
+          Row(
+            children: [
+              InfoIcon(
+                  iconData: Icons.group, cantidad: 400, indication: "Patients"),
+              InfoIcon(
+                  iconData: Icons.show_chart,
+                  cantidad: 400,
+                  indication: "Years expert"),
+              InfoIcon(
+                  iconData: Icons.star, cantidad: 400, indication: "Rating"),
+              InfoIcon(
+                  iconData: Icons.chat, cantidad: 400, indication: "Reviwes")
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, left: 8.0),
+            child: Row(children: [Text("About Me")]),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                      width: 380,
+                      height: 200,
+                      child: Text(
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                        textAlign: TextAlign.justify,
+                      )),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.blue,
+              child: ButtonBar(children: [
+                Icon(
+                  Icons.call,
+                  color: Colors.white,
+                ),
+                Text("Start Voice Call (14.30 - 15.00 PM)")
+              ]),
+            ),
+          )
         ],
       ),
     );
